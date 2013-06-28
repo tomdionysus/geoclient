d4f56d7a4da5dffcc62d81b3c3b25e4826b7669c
Tom Cully <tomhughcully@gmail.com>
2013-06-28 17:21:18 +1000

    Restoring settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -287,5 +287,4 @@ module.exports = function (grunt) {
      */
     grunt.registerTask('run', ['build', 'testServer', 'watch']);
     grunt.registerTask('test', ['build', 'jasmine']);
-    grunt.registerTask('heroku', ['build']);
 };
